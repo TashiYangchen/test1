1cf9d6e0c17076c5fc0e4e3b771fe12352019d02
unknown <02230084.cst@rub.edu.bt>
2024-03-01 16:13:25 +0600

    logical example

---
+++ logical .py
@@ -0,0 +1,14 @@
+# Example 1
+x = 5
+print(x>3 and x<10) #True because both conditions are True
+# Example 2
+y = 12 
+print(y<10 or y%2 == 0) #True because the second condition is True
+
+## Not operator
+# Example 1
+x = 5
+print(not(x>3 and x<10)) #False because the condition inside the not is true
+# Example 2
+y = 12 
+print(not(y<10 or y%5 == 0)) #True because the condition inside the not is false
